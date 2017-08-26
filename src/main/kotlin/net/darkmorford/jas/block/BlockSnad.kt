0a0329442bf4a651247ee3f7f9fd39505c31d958
rebelliousuno <RebelliousUno@gmail.com>
2017-08-26 21:21:02 +0100

    chore(): Code Clean Up

--- src/main/kotlin/net/darkmorford/jas/block/BlockSnad.kt
+++ src/main/kotlin/net/darkmorford/jas/block/BlockSnad.kt
@@ -6,24 +6,18 @@ import net.minecraft.block.BlockCactus
 import net.minecraft.block.BlockReed
 import net.minecraft.block.BlockSand
 import net.minecraft.block.SoundType
-import net.minecraft.block.material.MapColor
 import net.minecraft.block.material.Material
 import net.minecraft.block.properties.PropertyEnum
 import net.minecraft.block.state.BlockStateContainer
 import net.minecraft.block.state.IBlockState
 import net.minecraft.creativetab.CreativeTabs
-import net.minecraft.entity.player.EntityPlayer
-import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.util.EnumFacing
 import net.minecraft.util.math.BlockPos
-import net.minecraft.util.math.RayTraceResult
 import net.minecraft.world.IBlockAccess
 import net.minecraft.world.World
 import net.minecraftforge.common.EnumPlantType
 import net.minecraftforge.common.IPlantable
-import net.minecraftforge.fml.relauncher.Side
-import net.minecraftforge.fml.relauncher.SideOnly
 import java.util.*
 
 class BlockSnad : BlockSand(), IMetaBlockSnad {
