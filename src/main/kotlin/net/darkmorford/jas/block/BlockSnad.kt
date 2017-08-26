ecba41246cb45d144aaa04fdd37d1f1718551a7e
rebelliousuno <RebelliousUno@gmail.com>
2017-08-26 10:44:19 +0100

    feat(): Implement sustaining plants

--- src/main/kotlin/net/darkmorford/jas/block/BlockSnad.kt
+++ src/main/kotlin/net/darkmorford/jas/block/BlockSnad.kt
@@ -3,11 +3,17 @@ package net.darkmorford.jas.block
 import net.darkmorford.jas.JustAnotherSnad
 import net.darkmorford.jas.item.IMetaBlockSnad
 import net.minecraft.block.SoundType
+import net.minecraft.block.material.Material
 import net.minecraft.block.properties.PropertyEnum
 import net.minecraft.block.state.BlockStateContainer
 import net.minecraft.block.state.IBlockState
 import net.minecraft.creativetab.CreativeTabs
 import net.minecraft.item.ItemStack
+import net.minecraft.util.EnumFacing
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.IBlockAccess
+import net.minecraftforge.common.EnumPlantType
+import net.minecraftforge.common.IPlantable
 
 class BlockSnad : AbsBlockSnad(), IMetaBlockSnad {
 
@@ -25,6 +31,23 @@ class BlockSnad : AbsBlockSnad(), IMetaBlockSnad {
 
 	override fun getMetaFromState(state: IBlockState): Int {
 		return state.getValue(VARIANT).metadata
+	}
+
+	override fun canSustainPlant(state: IBlockState, world: IBlockAccess, position: BlockPos, facing : EnumFacing, plantable: IPlantable): Boolean {
+		val plantPosition = BlockPos(position.x, position.y + 1, position.z)
+		val plantType = plantable.getPlantType(world, plantPosition)
+
+		return when (plantType) {
+			EnumPlantType.Desert -> true
+			EnumPlantType.Water -> (world.getBlockState(position).material  == Material.WATER) && (world.getBlockState(position) == defaultState)
+			EnumPlantType.Beach -> (
+					(world.getBlockState(BlockPos(position.x - 1, position.y, position.z)).material == Material.WATER) ||
+							(world.getBlockState(BlockPos(position.x + 1, position.y, position.z)).material == Material.WATER) ||
+							(world.getBlockState(BlockPos(position.x, position.y, position.z + 1)).material == Material.WATER) ||
+							(world.getBlockState(BlockPos(position.x, position.y, position.z - 1)).material == Material.WATER)
+					)
+			else -> false
+		}
 	}
 
 	init {
