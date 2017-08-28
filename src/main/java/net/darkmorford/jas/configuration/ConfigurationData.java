9dda32cbf9f247bf8812a419ec4cbbfd33a5cad5
rebelliousuno <RebelliousUno@gmail.com>
2017-08-28 21:33:03 +0100

    fix(): Correct Typo

--- src/main/java/net/darkmorford/jas/configuration/ConfigurationData.java
+++ src/main/java/net/darkmorford/jas/configuration/ConfigurationData.java
@@ -2,7 +2,7 @@ package net.darkmorford.jas.configuration;
 
 public class ConfigurationData {
 	public static final int SNAD_SPEED_INCREASE_DEFAULT = 2;
-	public static final String SNAD_SPEED_INCREASE_KEY = "Snad Speed increase multilpier";
+	public static final String SNAD_SPEED_INCREASE_KEY = "Snad speed increase multiplier";
 	public static final int SOUL_SNAD_SPEED_INCREASE_DEFAULT = 4;
 	public static final String SOUL_SNAD_SPEED_INCREASE_KEY = "Soul Snad speed increase multiplier";
 
